514f6727f1b196ffa286f6d392c33961201d1498
T-G3EK-T <oussamawahi123@gmail.com>
2023-09-21 03:39:06 +0100

    la bghiti

--- console.py
+++ console.py
@@ -25,39 +25,11 @@ class HBNBCommand(cmd.Cmd):
         """
         pass
     
-    def do_create(self, line):
+    def do_create(self, arg):
         """
-        Usage: katcreayi <class> <key1> = <value1> <key2> = <value2> ...
+        Usage: create <Class name> <param 1> <param 2> <param 3>...
+        arg: <key name>=<value> 
         """
-        try:
-            if not line:
-                raise SyntaxError("No input provided")
-    
-            parts = line.split(" ")
-            class_name = parts[0]
-            args = parts[1:]
-    
-            kwargs = {}
-            for arg in args:
-                key, value = arg.split("=")
-                value = value.strip().strip('"').replace("_", " ")
-    
-                kwargs[key] = value
-    
-            if not kwargs:
-                obj = eval(class_name)()
-            else:
-                obj = eval(class_name)(**kwargs)
-                # Assuming `storage` is defined somewhere
-                storage.new(obj)
-    
-            print(obj.id)
-            obj.save()
-    
-        except SyntaxError:
-            print("** Class name or input format is missing or incorrect **")
-        except NameError:
-            print("** Class doesn't exist **")
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
