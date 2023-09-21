99dc354c1e622dd2babf8e990a5094df3fcfaab0
T-G3EK-T <oussamawahi123@gmail.com>
2023-09-21 01:50:57 +0100

    tajriba

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 0x00. AirBnB clone - The console
+    Author : Oussama Wahi
     fil a7lam
--- console.py
+++ console.py
@@ -24,7 +24,40 @@ class HBNBCommand(cmd.Cmd):
         Do nothing on an empty line
         """
         pass
-
+    
+    def do_create(self, line):
+        """
+        Usage: katcreayi <class> <key1> = <value1> <key2> = <value2> ...
+        """
+        try:
+            if not line:
+                raise SyntaxError("No input provided")
+    
+            parts = line.split(" ")
+            class_name = parts[0]
+            args = parts[1:]
+    
+            kwargs = {}
+            for arg in args:
+                key, value = arg.split("=")
+                value = value.strip().strip('"').replace("_", " ")
+    
+                kwargs[key] = value
+    
+            if not kwargs:
+                obj = eval(class_name)()
+            else:
+                obj = eval(class_name)(**kwargs)
+                # Assuming `storage` is defined somewhere
+                storage.new(obj)
+    
+            print(obj.id)
+            obj.save()
+    
+        except SyntaxError:
+            print("** Class name or input format is missing or incorrect **")
+        except NameError:
+            print("** Class doesn't exist **")
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
